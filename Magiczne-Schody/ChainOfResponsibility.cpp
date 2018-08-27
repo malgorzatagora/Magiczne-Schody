33c8bd015ce94d19c56a68a067a89341f7fef033
Mateusz <mateuszurbanek@gmail.com>
2018-08-27 23:28:31 +0200

    Dodanie pliku nagłówkowego i źródłowego dla Łańcucha zobowiązań

---
+++ Magiczne-Schody/ChainOfResponsibility.cpp
@@ -0,0 +1,10 @@
+/*******************************************************
+* Copyright (C) 2018 Mateusz Urbanek <mateuszurbanek@gmail.com>
+*
+* This file is part of project Magiczne Schody.
+*
+* Project Magiczne Schody can not be copied and/or distributed without the express
+* permission of Malgorzata Gora <gora.malgorzata.b@gmail.com>
+*******************************************************/
+
+// Chain of responsibility placeholder.
