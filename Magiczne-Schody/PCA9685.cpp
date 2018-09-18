8e93f97817b39c458c31573a633a47907a39ace0
malgorzatagora <gora.malgorzata.b@gmail.com>
2018-09-18 03:44:34 +0200

    PCA_szkielet

--- Magiczne-Schody/PCA9685.cpp
+++ Magiczne-Schody/PCA9685.cpp
@@ -2,12 +2,37 @@
 #include "PCA9685.h"
 
 
-PCA9685::PCA9685(I2C *i2c)
+PCA9685::PCA9685(I2C *i2c, int address)
 {
 	this->i2c = i2c;
+	this->address = address;
 }
 
 
 PCA9685::~PCA9685()
 {
 }
+
+void PCA9685::GoToSleep(void)
+{}
+void PCA9685::Restart(void)
+{}
+void PCA9685::Init(void)
+{}
+void PCA9685::Reset(void)
+{}
+void PCA9685::SetPWMFreq(int frequency)
+{}
+void PCA9685::SetMode1(int newMode)
+{}
+void PCA9685::SetMode2(int newMode)
+{}
+void PCA9685::SetPin(int pinNumber, int dutyCycle)
+{}
+int PCA9685::ReadRegister(int registerNumber)
+{
+	return 0;
+}
+void PCA9685::WriteRegister(int registerNumber, int value)
+{}
+
--- Magiczne-Schody/PCA9685.h
+++ Magiczne-Schody/PCA9685.h
@@ -1,13 +1,37 @@
 #pragma once
 #include "mbed.h"
+#define MODE1_ADDRESS	0x00
+#define MODE2_ADDRESS	0x01
+#define MODE1_SLEEP		0x10
+#define MODE1_EXTCLK	0x40
+#define MODE1_AI		0x20
+#define MODE1_ALLCALL	0x01
+//...
+
+/*#define LED0_ON_L		0x06
+#define LED0_ON_H		0x07
+#define LED0_OFF_L		0x08
+#define LED0_OFF_H		0x09
+*/
 
 class PCA9685
 {
 public:
-	PCA9685(I2C *i2c);
+	PCA9685(I2C *i2c, int address);
 	~PCA9685();
+	void GoToSleep(void);
+	void Restart(void);
+	void Init(void);
+	void Reset(void);
+	void SetPWMFreq(int frequency);
+	void SetMode1(int newMode);
+	void SetMode2(int newMode);
+	void SetPin(int pinNumber, int dutyCycle);
+	int ReadRegister(int registerNumber);
 
 private:
 	I2C * i2c;
+	int address;
+	void WriteRegister(int registerNumber, int value);
 };
 
